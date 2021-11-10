7ecdd410edbd8ff6bd0ca650cec2935d637f6351
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-10 18:18:01 +0200

    All main tests passed

--- src/python_functions_practice.py
+++ src/python_functions_practice.py
@@ -31,4 +31,14 @@ def number_to_full_month_name(number):
 
     elif number == 9:
         return "September"
+
+def number_to_short_month_name(number):
+    if number == 1:
+        return "Jan"
+
+    elif number == 4:
+        return "Apr"
+
+    elif number == 10:
+        return "Oct"
     
